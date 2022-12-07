351cbee182c6c71da5953ce16e7d3edc4ca6b37d
Venuja <93823328+VenujaK@users.noreply.github.com>
2022-12-07 11:23:46 +0530

    Web chat Inserted

--- Offers.php
+++ Offers.php
@@ -27,4 +27,7 @@
 </div>
 <?php @include("./Components/Footer.php") ?>
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </html>
--- Staff_Interface.php
+++ Staff_Interface.php
@@ -5,103 +5,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style>
-       @import url('htpps://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900');
-*{
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: 'Poppins', sans-serif;
-}
-body{
-display: flex;
-justify-content: center;
-  align-items: center;
-  min-height: 100vh;
-  background: #0e1538;
-}
-.container{
-display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-}
-.container a{
-position: relative;
-  width: 160px;
-  height: 60px;
-  display: inline-block;
-  background: #fff;
-  margin: 20px;
-}
-.container a:before,
-.container a:after
-{
-content:'';
-  position: absolute;
-  inset: 0;
-  transition: 0.5s;
-  background: #f00
-}
-.container a:nth-child(1):before,
-.container a:nth-child(1):after
-{
- background: linear-gradient(45deg, #FF6B00, #0e1538, #FFE604)
-}
-.container a:nth-child(2):before,
-.container a:nth-child(2):after
-{
-  background: linear-gradient(45deg, #FFE604, #0e1538, #FF6B00);
-}
-.container a:hover:before
-{
-  inset: -3px;  
-}
-.container a:hover:after
-{
-  inset: -3px;  
-  filter: blur(10px);
-}
-.container a span{
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  display: inline-block;
-  background: #0e1538;
-  z-index: 10;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 1.2em;
-  text-transform: uppercase;
-  letter-spacing: 2px;
-  color: #fff;
-  border: 1px solid #040a29;
-  overflow: hidden;
-}
-.container a span::before{
-content:'';
-  position: absolute;
-  top: 0;
-  left: -50%;
-  width: 100%;
-  height: 100%;
-  background: rgba(255,255,255,0.075);
-    transform: skew(25deg)
-}
-    </style>
+    <link rel="stylesheet" href="./CSS/Staff_Interface.css">
+    <link rel="stylesheet" href="./CSS/Offers.css">
+    <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
 </head>
 <body>
 <div class="container">
-    <a href="#">
-      <span>Button</span>
+    <a href="./WebChat.html">
+      <span>Queries</span>
     </a>
     <a href="#">
-      <span>Button</span>
+      <span>Bookings</span>
+    </a>
+    <a href="#">
+      <span>Availability</span>
     </a>
     
   </div>
+  
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </html>
