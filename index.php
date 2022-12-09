e8619a895030c748e3f931f0c9dfab241225b50f
Venuja <93823328+VenujaK@users.noreply.github.com>
2022-12-09 10:18:37 +0530

    Index page cards updated

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 </head>
 
 <body>
-<?php @include("./Components/Header.php") ?>
+    <?php @include("./Components/Header.php") ?>
     <!-- Nav bar ends -->
     <!-- Hero section -->
     <div class="container  hero">
@@ -28,6 +28,7 @@
                 <div class="card " style="width: 18rem;">
                     <img src="./Images/carousel 1.png" class="card-img-top" alt="...">
                     <div class="card-body">
+                        <h3 class="crdhead">Villas</h3>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         <button type="button" onclick="booking()" class="btn btn-outline-warning">Book Now</button>
                     </div>
@@ -35,8 +36,9 @@
             </div>
             <div class="col d-flex justify-content-center">
                 <div class="card" style="width: 18rem;">
-                    <img src="./Images/carousel 2.png" class="card-img-top" alt="...">
+                    <img src="https://nimvo.com/wp-content/uploads/2017/08/Purple-Bedroom-750x531.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
+                        <h3 class="crdhead">Duluxe</h3>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         <button type="button" onclick="booking()" class="btn btn-outline-warning">Book Now</button>
                     </div>
@@ -44,8 +46,9 @@
             </div>
             <div class="col d-flex justify-content-center">
                 <div class="card" style="width: 18rem;">
-                    <img src="./Images/carousel 3.png" class="card-img-top" alt="...">
+                    <img src="https://i.pinimg.com/736x/c6/6f/b7/c66fb7b279a660e3daf84e7d32efdf07--luxury-rooms-luxury-living.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
+                        <h3 class="crdhead">Luxury</h3>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         <button type="button" onclick="booking()" class="btn btn-outline-warning">Book Now</button>
                     </div>
@@ -53,8 +56,9 @@
             </div>
             <div class="col d-flex justify-content-center">
                 <div class="card" style="width: 18rem;">
-                    <img src="http://island.lk/wp-content/uploads/2022/04/Granbell-Hotel.jpg" class="card-img-top" alt="...">
+                    <img src="https://content.r9cdn.net/himg/70/de/74/ice-98111-photo.aspx_did=2692_brochureid=98111_publicid=37388724_instanceid=10-image.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
+                        <h3 class="crdhead">Normal</h3>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         <button type="button" onclick="booking()" align="center" class="btn btn-outline-warning ">Book Now</button>
                     </div>
@@ -68,7 +72,7 @@
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 <script>
-    function booking(){
+    function booking() {
         window.location.replace("Client_Room_View.php");
     }
 </script>
